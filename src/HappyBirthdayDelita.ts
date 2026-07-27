18cad28bc6b186a87550d9ea906e29cc011b91cb
Tresko. <jegerenskuffasokk@gmail.com>
2026-07-27 12:53:31 +0200

    corrected secondsLeft var

--- src/HappyBirthdayDelita.ts
+++ src/HappyBirthdayDelita.ts
@@ -30,7 +30,7 @@ function countdown(){
 
 function calculate(): Countdown{
     const currTime = new Date();
-    let secondsLeft = BIRTHDAY_DATE.getTime() - currTime.getTime();
+    let secondsLeft = Math.floor(BIRTHDAY_DATE.getTime() - currTime.getTime() /1000);
 
     if(secondsLeft <= 0){
         birthday(); // should change the visuals
